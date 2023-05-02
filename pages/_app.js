31a7c4f630674ddaed60d90aa4359d6a6021c1cd
Anup Sheth <anup.sheth@gmail.com>
2023-05-01 20:34:13 -0400

    component case change

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import ShopProvider from "../context/shopContext";
 import "../styles/globals.css";
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react"
-import ProductCard from "../components/productcard"
+import ProductCard from "../components/ProductCard"
 
 import {  callShopify, AllProducts } from "../helpers/shopify"
 
